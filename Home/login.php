06cdaa4f80342b655e1fbf4b23ea37e7cd6cce66
Shola Abhus <sholaabhus@gmail.com>
2021-04-17 23:48:43 +0100

    update

--- Home/login.php
+++ Home/login.php
@@ -1,44 +1,36 @@
 
   
 <?php 
-  $username = $_POST['username']?? "";    // create variable answer1
+  $username = $_POST['username']?? "";    // create variable 
   $password = $_POST['password']?? "";
-  $username ='admin';
-  $password ='admin@@';
  $hashpassword =password_hash($password,PASSWORD_BCRYPT); // hash password
   $error = "";
   $success ="";
-  $good ="";
+ 
   
   if(isset($_POST['submit'])) {// checks whether user clicked submit button or not
-    
-    $f=fopen("data.txt","r+"); //open text file
+    if (!preg_match("#[0-9]+#", $password)) {
+      $error ="Your Password must contain at least 1 number";
+    }elseif(strlen($_POST['password']) < 5 ) {     // check password length
+   $error = "Password must be at least 6 characters";
+    }else{
+      $success ="Thanks I have received your Logging details";
+    $f=fopen("data.txt","a+"); //open text file
     fwrite($f, 'Your login name is:'. $username );
     fwrite($f, "\n");
     fwrite($f, 'Your password is:'. $password);
     fwrite($f, "\n");
     fwrite($f, 'bcrypt algorithm:' . $hashpassword);
-
-    
- if(password_verify($_POST['password'],$hashpassword)) { //check if plain text password matches hashpassword 
-  $success ="Logged in Successful";
-  } else {
-  $error ="Wrong username or password";
- }
- 
- if(strlen($_POST['password']) < 5 ) {     // check password length
-  $error = "Password must be at least 6 characters";
-}
-
-
-   if(empty($_POST['username'])  && empty($_POST['password'])){  //check if fields are empty
- $error ="Please enter your username and password";
-  }
-    
- 
+    fclose($f);
   
-fclose($f);
-}
+    }
+ 
+  if(empty($_POST['username'])  && empty($_POST['password'])){  //check if fields are empty
+    $error ="Please enter your username and password";
+     
+  }
+
+  }
 
     
   ?>
@@ -61,12 +53,13 @@ fclose($f);
     
     <h2>Please Login</h2> 
   <div class ="error"> <?php echo $error; ?></div><div class ="success"><?php echo $success; ?></div><br>
-
-   UserName: <input type ="text" name="username" placeholder="Enter username"><br><br>
+   UserName: <input type ="username" name="username" placeholder="Enter username"><br><br>
    Password: <input type ="password" name="password" placeholder="Enter password"><br><br>
  
    <button type ="submit" name="submit" value="submit" >Login</button>
- </div>
+
+  </div>
+  
  </form>
 
  <?php include "footer.html"; ?>
--- Home/movies.php
+++ Home/movies.php
@@ -14,16 +14,18 @@
     <div class="container">
     
     <h2>Movies Directory</h2> 
-    <p> Enter a genre to see filmname, Directory, Year of release and Rating</p>
-   
+
+    <p> Enter a genre to view  Filmname(s),Year of release, Rating and Director.</p>
+      
     Enter a Genre: <input type ="text" name="Genre" placeholder="Enter a Genre">  
-   
-   <br\>
+
    <input type ="submit" name="submit" value="submit" />
+   
+
  </div>
  </form>
-<br/>
-
+ <br/>
+ 
      
 <?php
 
@@ -41,70 +43,56 @@ public $director;
 public $genre;
 
 
+ function __construct($aFilmname, $aYearofrelease, $aRating, $aDirector, $aGenre) {
+     $this->filmname = $aFilmname;
+     $this->yearofrelease = $aYearofrelease;
+     $this->rating = $aRating;
+     $this->director = $aDirector;
+     $this-> genre = $aGenre;
 
-    
+ }
+ 
+
+   
 public function iterateAllProperties()
-   {
+  {
        foreach ($this as $key => $value) {
 
-       $input = $_POST['Genre'];
+      $input = $_POST['Genre'];
        
-       
-       if($input == $this->genre){
-       // echo "$key.$value  <br /> ";
-        echo(ucfirst("$key:$value <br />"));
-    
-      }
-        
+      if($input == $this->genre){
+       //echo (ucfirst("$key:$value <br />"));
+
+echo "<p class='number'> $key:$value </p>";
+
+
           
          }
-          
+        }    
         
-        }  
-
+       }  
     }
-$movie1 = new Movie; // object - instance of class
+     
+$movie1 = new Movie("The Adventures of Robin Hood", 1983, "PG-13","Michael Curtiz", "Family");// object - instance of class
 
-$movie1->filmname ='The Adventures of Robin Hood';
-$movie1->director ='Michael Curtiz ';
-$movie1->yearofrelease ='1983';
-$movie1->rating = 5;
-$movie1->genre ='Family';
+$movie2 = new Movie ("Snatch", 2000, "R", "Guy Ritchie", "Action");  // object - instance of class
 
+$movie3= new Movie("Gladiator", 2000, "R", "Ridley Scott","Action");  // object - instance of class
 
-$movie2 = new Movie;  // object - instance of class
+$movie4= new Movie("Harry Potter", 2001, 5, "David Yates", "Fantasy");  // object - instance of class
 
-$movie2->filmname ='Snatch';
-$movie2->director = 'Guy Ritchie';
-$movie2->yearofrelease ='2000';
-$movie2->rating = 4;
-$movie2->genre ='Action';
-
-
-
-$movie3= new Movie;  // object - instance of class
-
-$movie3->filmname ='Gladiator';
-$movie3->director = 'Ridley Scott';
-$movie3->yearofrelease ='2000';
-$movie3->rating = 5;
-$movie3->genre ='Action';
-
-$movie4= new Movie;  // object - instance of class
-
-$movie4->filmname ='Harry Potter';
-$movie4->director = 'David Yates, Chris Columbus';
-$movie4->yearofrelease ='2001';
-$movie4->rating = 5;
-$movie4->genre ='Fantasy';
 
 $movie1->iterateAllProperties();
-echo "<br>";
+//echo "<br>";
 $movie2->iterateAllProperties();
-echo "<br>";
+//echo "<br>";
 $movie3->iterateAllProperties();
-echo"<br>";
+//echo"<br>";
 $movie4->iterateAllProperties();
+//echo"<br>";
+
+
+
 
 
 
@@ -114,6 +102,6 @@ $movie4->iterateAllProperties();
 
 
 ?>
-
+<?php include "footer.html"; ?>
 </body>
  </html>
